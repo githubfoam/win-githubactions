854be42cb0c24cbc8784db6c2843fcbdfeb91f68
Your Name <you@example.com>
2021-07-19 15:21:57 +0300

    init dfir

--- scripts/install_osquery.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-brew update 
-brew install osquery
-
-osqueryi --json 'select cpu_type, cpu_brand, hardware_vendor, hardware_model from system_info;' > system_info.json
-cat system_info.json
