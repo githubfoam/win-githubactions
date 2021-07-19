f98c6348046a966e7076c0c809d08da561b9fb1d
Your Name <you@example.com>
2021-07-19 16:54:51 +0300

    deleted_executable

--- scripts/DFIR_Osquery.ps1
+++ scripts/DFIR_Osquery.ps1
@@ -6,6 +6,13 @@ $VerbosePreference = "continue"
 Write-Output $VerbosePreference
 #--------------------------------------------------------------------------------------------------------
 
+Write-Host "#################################################"
 osqueryi --json 'select * from listening_ports ;' > listening_ports.json
 Get-Content -Path listening_ports.json
+
+Write-Host "#################################################"
+# Check the processes that have a deleted executable
+osqueryi --json 'SELECT * FROM processes WHERE on_disk = 0' > deleted_executable.json
+Get-Content -Path deleted_executable.json
+
 
